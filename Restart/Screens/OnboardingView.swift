093c39b323b3787b65aa8c0119b24d6d2afbbbba
rfrancodev <francorfs@icloud.com>
2024-09-30 21:13:35 -0300

    Functions of the button completed

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -11,7 +11,10 @@ struct OnboardingView: View {
     //MARK: - PROPETIES
 
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
     //MARK: - BODY
 
     var body: some View {
@@ -74,7 +77,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
     
                         Spacer()
                     }
@@ -92,15 +95,28 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80{
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                                .onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2{
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                }
+                        )//: GESTURE
                         
                         Spacer()
                     }//:HSTACK
-                    
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             }  //: VSTACK
         }  //: ZSTACK
