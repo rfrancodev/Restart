089907031913f870dbc4e714f38bfe95a068b18c
rfrancodev <francorfs@icloud.com>
2024-10-03 20:08:39 -0300

    Parallax Effect created

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -15,6 +15,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Generosidade."
     
     //MARK: - BODY
 
@@ -26,10 +29,12 @@ struct OnboardingView: View {
                 //MARK: - HEADER
                 Spacer()
                 VStack(spacing: 0) {
-                    Text("Generosidade.")
+                    Text(textTitle)
                         .font(.system(size: 52))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text(
                         """
@@ -51,13 +56,51 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1:0)
-                        .animation(.easeInOut(duration: 0.5), value: isAnimating)
+                        .animation(.easeInOut(duration: 0.5),
+                        value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0 )
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Doar."
+                                        }
+                                    }
+                                }
+                                .onEnded { _ in
+                                    imageOffset.width = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Generosidade."
+                                    }
+                                }
+                        )//: GESTURE
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 }  //: CENTER
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1: 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 
                 Spacer()
                 
